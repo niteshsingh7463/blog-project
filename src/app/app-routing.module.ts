010d384bcbb492eed36025a531dc886be8098efe
nitesh <nitesh@quicksand.com>
2021-12-24 00:25:12 +0530

    index-blog page completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndexBlogComponent } from './components/index-blog/index-blog.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: '',
+		component: IndexBlogComponent
+	}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [ RouterModule.forRoot(routes) ],
+	exports: [ RouterModule ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ScrollToModule } from '@nicky-lenaers/ngx-scroll-to';
+import { FeatherModule } from 'angular-feather';
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import { allIcons } from 'angular-feather/icons';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexBlogComponent } from './components/index-blog/index-blog.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { SwitcherComponent } from './shared/switcher/switcher.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [ AppComponent, IndexBlogComponent, FooterComponent, SwitcherComponent, HeaderComponent ],
+	imports: [
+		BrowserModule,
+		BrowserAnimationsModule,
+		AppRoutingModule,
+		FeatherModule.pick(allIcons),
+		ScrollToModule.forRoot(),
+		CarouselModule
+	],
+	providers: [],
+	bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
